43eba2e0196bd42128a1bc062cdcbe2ae35f363b
sarahchrstne <sarah.christinee20@gmail.com>
2021-10-05 23:45:40 +0700

    Push LeafletJS task
    Test Practical Developer Task from PT. Bhumi Varta Technology

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function(req, res) {
+  res.render('view');
+});
+
+
+app.listen(8686);
+console.log('Server is listening on port 8686');
